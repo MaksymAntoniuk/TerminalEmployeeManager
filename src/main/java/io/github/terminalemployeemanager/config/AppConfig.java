f6e883afc95f8ad87d3ff6640f48b731a4cfa9c2
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2024-08-09 22:02:31 +0200

    Initial project setup and create configuration for a database

---
+++ src/main/java/io/github/terminalemployeemanager/config/AppConfig.java
@@ -0,0 +1,28 @@
+package io.github.terminalemployeemanager.config;
+
+
+import com.zaxxer.hikari.util.DriverDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/employees_spring");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Maksym2003");
+        return dataSource;
+    }
+
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+}
