be787b7a32a3d428b8430ea9148bd4756fcdddab
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2024-08-16 18:35:51 +0200

    Add a new class with method which formats data when a user wants to see all employees

---
+++ src/main/java/io/github/terminalemployeemanager/util/EmployeeFormatter.java
@@ -0,0 +1,20 @@
+package io.github.terminalemployeemanager.util;
+
+import io.github.terminalemployeemanager.Model.Employee;
+
+import java.util.List;
+
+public class EmployeeFormatter {
+    public static void printEmployeeList(List<Employee> employees) {
+        if (employees.isEmpty()) {
+            System.out.println("No employees found.");
+            return;
+        }
+
+        System.out.printf("%-5s %-20s %-15s%n", "ID", "Name", "Role");
+        System.out.println("------------------------------");
+        for (Employee employee : employees) {
+            System.out.printf("%-5d %-20s %-15s%n", employee.getId(), employee.getName(), employee.getRole());
+        }
+    }
+}
