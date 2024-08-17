d756f57c01e87dbd361bed8b0e3709b910f8e1dc
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2024-08-17 13:18:51 +0200

    Add dependency injection for EmployeeService by @Autowired

--- src/main/java/io/github/terminalemployeemanager/Main.java
+++ src/main/java/io/github/terminalemployeemanager/Main.java
@@ -2,137 +2,82 @@ package io.github.terminalemployeemanager;
 
 import io.github.terminalemployeemanager.Model.Employee;
 import io.github.terminalemployeemanager.service.EmployeeService;
+import io.github.terminalemployeemanager.util.EmployeeFormatter;
+import io.github.terminalemployeemanager.util.MenuApp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.List;
+import java.util.InputMismatchException;
 import java.util.Optional;
 import java.util.Scanner;
 
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "io.github.terminalemployeemanager.repository")
 public class Main {
+
+    @Autowired
+    private EmployeeService employeeService;
+
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext("io.github.terminalemployeemanager");
-        EmployeeService employeeService = context.getBean(EmployeeService.class);
+        ApplicationContext context = SpringApplication.run(Main.class, args);
 
+        Main app = context.getBean(Main.class);
+        app.runApplication();
+    }
+
+    public void runApplication() {
         Scanner scanner = new Scanner(System.in);
-        while (true){
-            System.out.print("""
-                    1. List all employees
-                    2. Find employee by ID
-                    3. Add new employee
-                    4. Delete employee
-                    Enter your choice: 
-                    """);
-            int choice = scanner.nextInt();
+        while (true) {
+            try {
+                MenuApp.showMenu();
+                int choice = scanner.nextInt();
+                scanner.nextLine();
 
-            switch (choice){
-                case 1:
-                    employeeService.getAllEmployees().forEach(System.out::println);
-                    break;
-                case 2:
-                    System.out.print("Enter employee ID: ");
-                    int id = scanner.nextInt();
-                    Optional<Employee> employee = employeeService.getEmployeeById(id);
-                    if (employee != null){
-                        System.out.println(employee);
-                    } else {
-                        System.out.println("Employee not found!");
-                    }
-                    break;
-                case 3:
-                    System.out.print("Name: ");
- package io.github.terminalemployeemanager;
-
-import io.github.terminalemployeemanager.Model.Employee;
-import io.github.terminalemployeemanager.service.EmployeeService;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
-
-                    public class Main {
-                        public static void main(String[] args) {
-                            ApplicationContext context = new AnnotationConfigApplicationContext("io.github.terminalemployeemanager");
-                            EmployeeService employeeService = context.getBean(EmployeeService.class);
-
-                            Scanner scanner = new Scanner(System.in);
-                            while (true){
-                                System.out.print("""
-                    1. List all employees
-                    2. Find employee by ID
-                    3. Add new employee
-                    4. Delete employee
-                    Enter your choice: 
-                    """);
-                                int choice = scanner.nextInt();
-
-                                switch (choice){
-                                    case 1:
-                                        employeeService.getAllEmployees().forEach(System.out::println);
-                                        break;
-                                    case 2:
-                                        System.out.print("Enter employee ID: ");
-                                        int id = scanner.nextInt();
-                                        Optional<Employee> employee = employeeService.getEmployeeById(id);
-                                        if (employee != null){
-                                            System.out.println(employee);
-                                        } else {
-                                            System.out.println("Employee not found!");
-                                        }
-                                        break;
-                                    case 3:
-                                        System.out.print("Name: ");
-                                        String name = scanner.next().toString();
-                                        System.out.print("Role: ");
-                                        String role = scanner.next().toString();
-                                        Employee employee1 = new Employee(name, role);
-                                        employeeService.addEmployee(employee1);
-                                        break;
-                                    case 4:
-                                        System.out.println("Enter employee ID: ");
-                                        id = scanner.nextInt();
-                                        employeeService.deleteEmployee(id);
-                                        System.out.println("Employee with " + id + " successfully deleted.");
-                                        break;
-                                    case 5:
-                                        System.out.println("Exiting...");
-                                        scanner.close();
-                                        System.exit(0);
-                                        break;
-                                    default:
-                                        System.out.println("Invalid choice, please try again.");
-                                }
-
-
-                            }
-
+                switch (choice) {
+                    case 1:
+                        EmployeeFormatter.printEmployeeList(employeeService.getAllEmployees());
+                        System.out.println();
+                        break;
+                    case 2:
+                        System.out.print("Enter employee ID: ");
+                        Long id = scanner.nextLong();
+                        Optional<Employee> employee = employeeService.getEmployeeById(id);
+                        if (employee.isPresent()) {
+                            System.out.println(employee.get());
+                        } else {
+                            System.out.println("Employee not found!");
                         }
-                    }
-                    String name = scanner.next().toString();
-                    System.out.print("Role: ");
-                    String role = scanner.next().toString();
-                    Employee employee1 = new Employee(name, role);
-                    employeeService.addEmployee(employee1);
-                    break;
-                case 4:
-                    System.out.println("Enter employee ID: ");
-                    id = scanner.nextInt();
-                    employeeService.deleteEmployee(id);
-                    System.out.println("Employee with " + id + " successfully deleted.");
-                    break;
-                case 5:
-                    System.out.println("Exiting...");
-                    scanner.close();
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid choice, please try again.");
+                        break;
+                    case 3:
+                        System.out.print("Name: ");
+                        String name = scanner.nextLine();
+                        System.out.print("Role: ");
+                        String role = scanner.nextLine();
+                        employeeService.addEmployee(name, role);
+                        System.out.println("Employee successfully added.");
+                        break;
+                    case 4:
+                        System.out.print("Enter employee ID: ");
+                        id = scanner.nextLong();
+                        employeeService.deleteEmployee(id);
+                        System.out.println("Employee with " + id + " successfully deleted.");
+                        break;
+                    case 5:
+                        System.out.println("Exiting...");
+                        scanner.close();
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Invalid choice, please try again.");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+                scanner.nextLine();
             }
-
-
         }
-
     }
 }
