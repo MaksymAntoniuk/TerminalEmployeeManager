8f54e17e405b147011aabcef9c0f189626cde112
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2024-08-18 09:25:37 +0200

    Add new class with method for show in proper way table `info_employee`

---
+++ src/main/java/io/github/terminalemployeemanager/util/InfoEmployeeFormatter.java
@@ -0,0 +1,20 @@
+package io.github.terminalemployeemanager.util;
+
+import io.github.terminalemployeemanager.entity.InfoEmployee;
+
+import java.util.List;
+
+public class InfoEmployeeFormatter {
+    public static void printInfoEmployee(List<InfoEmployee> infoEmployees) {
+        if (infoEmployees.isEmpty()) {
+            System.out.println("No employees found.");
+            return;
+        }
+
+        System.out.printf("%-5s %-30s %-30s %-15s%n", "ID", "Email", "Phone", "Employee_ID");
+        System.out.println("-----------------------------------------------------------------------------");
+        for (InfoEmployee employee : infoEmployees) {
+            System.out.printf("%-5d %-30s %-30s %-15s%n", employee.getId(), employee.getEmail(), employee.getPhone(), employee.getEmployee().getId());
+        }
+    }
+}
