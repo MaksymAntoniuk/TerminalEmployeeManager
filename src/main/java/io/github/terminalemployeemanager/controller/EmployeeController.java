9572c6bb659ac580d868fb0f78dcaef64f21f6a8
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2024-08-19 22:06:18 +0200

    Delete setting for server port

--- src/main/java/io/github/terminalemployeemanager/controller/EmployeeController.java
+++ src/main/java/io/github/terminalemployeemanager/controller/EmployeeController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/employee")
