cefaa73de7467351eddfb0cdd3192ff918728098
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2024-08-19 20:51:39 +0200

    Add class for test

---
+++ src/test/java/io/github/terminalemployeemanager/TerminalEmployeeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.terminalemployeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalEmployeeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
