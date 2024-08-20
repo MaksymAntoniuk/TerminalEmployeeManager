c4caed7439519fcec4c2d38deabdca54e4520605
Maksym Antoniuk <118458067+MaksymAntoniuk@users.noreply.github.com>
2024-08-20 08:38:44 -0500

    Update Main.java

--- src/main/java/io/github/terminalemployeemanager/Main.java
+++ src/main/java/io/github/terminalemployeemanager/Main.java
@@ -9,49 +9,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext("io.github.terminalemployeemanager");
-        EmployeeService employeeService = context.getBean(EmployeeService.class);
-
-        Scanner scanner = new Scanner(System.in);
-        while (true){
-            System.out.print("""
-                    1. List all employees
-                    2. Find employee by ID
-                    3. Add new employee
-                    4. Delete employee
-                    Enter your choice: 
-                    """);
-            int choice = scanner.nextInt();
-
-            switch (choice){
-                case 1:
-                    employeeService.getAllEmployees().forEach(System.out::println);
-                    break;
-                case 2:
-                    System.out.print("Enter employee ID: ");
-                    int id = scanner.nextInt();
-                    Optional<Employee> employee = employeeService.getEmployeeById(id);
-                    if (employee != null){
-                        System.out.println(employee);
-                    } else {
-                        System.out.println("Employee not found!");
-                    }
-                    break;
-                case 3:
-                    System.out.print("Name: ");
- package io.github.terminalemployeemanager;
-
-import io.github.terminalemployeemanager.Model.Employee;
-import io.github.terminalemployeemanager.service.EmployeeService;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
-
                     public class Main {
                         public static void main(String[] args) {
                             ApplicationContext context = new AnnotationConfigApplicationContext("io.github.terminalemployeemanager");
