ed3c6028e9b25930b9e844af77a08e388c35c9c7
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2024-08-22 19:33:00 +0200

    Delete annotation and import which was caused by merging

--- src/main/java/io/github/terminalemployeemanager/model/Employee.java
+++ src/main/java/io/github/terminalemployeemanager/model/Employee.java
@@ -1,13 +1,10 @@
 package io.github.terminalemployeemanager.model;
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
