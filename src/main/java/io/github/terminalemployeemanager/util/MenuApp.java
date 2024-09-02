3f83d2ff08918c0390bb1657411484bb69d1a55c
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2024-09-02 11:16:05 +0200

    Add new feature into the menu

--- src/main/java/io/github/terminalemployeemanager/util/MenuApp.java
+++ src/main/java/io/github/terminalemployeemanager/util/MenuApp.java
@@ -3,13 +3,16 @@ package io.github.terminalemployeemanager.util;
 public class MenuApp {
     public static void showMenu(){
         System.out.println("""
+                    -----------------Menu-----------------
                     1. List of all employees
                     2. Find employee by ID
                     3. Add new employee
                     4. Delete employee
                     5. Add info for employee by ID
                     6. List of all information about employees
-                    7. Exit"""
+                    7. Add salary for employee by ID
+                    8. List of all salaries
+                    9. Exit"""
         );
         System.out.print("Enter your choice: ");
     }
